04bdc61c84a6616c70c76db065bd12617c5fde95
blevm <mjblevins7@gmail.com>
2018-04-27 00:07:04 -0400

    mb solution

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,29 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
-  attr_reader :songs
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+
+  attr_accessor :name, :songs
+  #attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def initialize
-    @@artists << self
-    @songs = []
-  end
+      super
+      @songs = []
+    end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
-
   def add_song(song)
-    @songs << song
+    self.class.all << song
+    self.songs << song
     song.artist = self
   end
 
@@ -36,8 +31,6 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
 end
+
+# Pry.start
